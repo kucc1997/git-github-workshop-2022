a3f875ba9d048136f364b17cfb90f92742928ab1
Sabin Thapa <sabint017@gmail.com>
2022-01-25 20:32:09 +0545

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "../css/UserCard.css";
+import { SocialIcon } from "react-social-icons";
 
 const Footer = () => {
   return (
     <div className="footer">
       <p> Copyright &copy; 2022 | KUCC | All Rights Reserved</p>
+      <div className="socials">
+        <SocialIcon
+          url="http://kucc.ku.edu.np/"
+          bgColor="#38c0ce"
+          style={{ height: 30, width: 30, marginRight: "15px" }}
+        />
+        <SocialIcon
+          url="https://www.facebook.com/kucc1997"
+          style={{ height: 30, width: 30, marginRight: "15px" }}
+        />
+        <SocialIcon
+          url="https://github.com/kucc1997"
+          style={{ height: 30, width: 30 }}
+        />
+      </div>
     </div>
   );
 };
