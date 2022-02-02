daaf7e755fb1290bf31ea0a514bbb35380c1a85d
Sabin Thapa <sabint017@gmail.com>
2022-02-02 19:16:14 +0545

    edit Card

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -19,7 +19,12 @@ const UserCard = ({ participant }) => {
           }
           style={{ height: 25, width: 25, marginLeft: -2 }}
         />
-        <a href={participant.githubProjectUrl} style={{ marginLeft: 4 }}>
+        <a
+          href={participant.githubProjectUrl}
+          style={{ marginLeft: 4 }}
+          target="_blank"
+          rel="noreferrer"
+        >
           Repository Link
         </a>
       </div>
